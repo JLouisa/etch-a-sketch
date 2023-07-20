00545dd5fa8c7c87c7e2881435002bf1d3bab39d
Jonathan Louisa <jonathan.louisa@gmail.com>
2023-07-20 14:27:27 -0400

    Add responsive padding and grid size

--- javascript.js
+++ javascript.js
@@ -2,14 +2,16 @@
 
 const container = document.querySelector('.middle');
 
+let grid = 100
+
 let divs = []
-for(n = 1; n <= 16; n++) {
+for(n = 1; n <= grid; n++) {
     divs[n] = document.createElement('div');
     divs[n].classList.add(`head${n}`, 'divs');
     container.appendChild(divs[n]);
 
         let square = [];
-        for(let i = 1; i <= 16; i++) {
+        for(let i = 1; i <= grid; i++) {
             square[i] = document.createElement('div');
             square[i].classList.add(`row${n}col${i}`, 'square');
             divs[n].appendChild(square[i]);}
@@ -18,5 +20,9 @@ for(n = 1; n <= 16; n++) {
 // Create hover effect
 const sketch = document.querySelectorAll('.square');
 sketch.forEach(square => square.addEventListener("mouseover", (event) => {
-    event.target.style.backgroundColor = "gold";}
+    event.target.style.backgroundColor = "green";}
   ));
+
+// Adjust Padding for square
+const test = document.querySelectorAll('.square');
+test.forEach(square => square.setAttribute('style', `padding: ${800/grid}px;`));
