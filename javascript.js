0d7752a6e7e7ac8d1a2ec3f91fc827c820586ead
Jonathan Louisa <jonathan.louisa@gmail.com>
2023-07-20 13:31:16 -0400

    Add lingering hover effect trail

--- javascript.js
+++ javascript.js
@@ -1,3 +1,5 @@
+// Create 16x16 squares
+
 const container = document.querySelector('.middle');
 
 let divs = []
@@ -6,9 +8,15 @@ for(n = 1; n <= 16; n++) {
     divs[n].classList.add(`head${n}`, 'divs');
     container.appendChild(divs[n]);
 
-        let first = [];
+        let square = [];
         for(let i = 1; i <= 16; i++) {
-            first[i] = document.createElement('div');
-            first[i].classList.add(`first${i}`, 'square');
-            divs[n].appendChild(first[i]);}
+            square[i] = document.createElement('div');
+            square[i].classList.add(`row${n}col${i}`, 'square');
+            divs[n].appendChild(square[i]);}
 }
+
+// Create hover effect
+const sketch = document.querySelectorAll('.square');
+sketch.forEach(square => square.addEventListener("mouseover", (event) => {
+    event.target.style.backgroundColor = "gold";}
+  ));
