07ba7d5dd76a8bba7371b8da5036e5ade792afac
Jonathan Louisa <jonathan.louisa@gmail.com>
2023-07-20 11:53:30 -0400

    Make loops to create html elements

--- javascript.js
+++ javascript.js
@@ -5,81 +5,39 @@ const firstLine = document.createElement('div');
     firstLine.classList.add('first', 'divs');
     container.appendChild(firstLine);
 
-const firstFirst = document.createElement('div');
-    firstFirst.classList.add('firstFirst', 'square');
-    firstLine.appendChild(firstFirst);
-
-const firstSecond = document.createElement('div');
-    firstSecond.classList.add('firstSecond', 'square');
-    firstLine.appendChild(firstSecond);
-
-const firstThird = document.createElement('div');
-    firstThird.classList.add('firstThird', 'square');
-    firstLine.appendChild(firstThird);
-
-const firstFourth = document.createElement('div');
-    firstFourth.classList.add('firstFourth', 'square');
-    firstLine.appendChild(firstFourth);
+    let first = [];
+    for(let i1 = 1; i1 <= 16; i1++) {
+        first[i1] = document.createElement('div');
+        first[i1].classList.add(`first${i1}`, 'square');
+        firstLine.appendChild(first[i1]);}
 
     // ------------- 2nd Line -------------
 const secondLine = document.createElement('div');
     secondLine.classList.add('second', 'divs');
     container.appendChild(secondLine);
 
-const secondFirst = document.createElement('div');
-    secondFirst.classList.add('secondFirst', 'square');
-    secondLine.appendChild(secondFirst);
-
-const secondSecond = document.createElement('div');
-    secondSecond.classList.add('secondSecond', 'square');
-    secondLine.appendChild(secondSecond);
-
-const secondThird = document.createElement('div');
-    secondThird.classList.add('secondThird', 'square');
-    secondLine.appendChild(secondThird);
-
-const secondFourth = document.createElement('div');
-    secondFourth.classList.add('secondFourth', 'square');
-    secondLine.appendChild(secondFourth);
+    let second = [];
+    for(let i2 = 1; i2 <= 16; i2++) {
+        first[i2] = document.createElement('div');
+        first[i2].classList.add(`second${i2}`, 'square');
+        secondLine.appendChild(first[i2]);}
 
 // ------------- 3rd Line -------------
 const thirdLine = document.createElement('div');
     thirdLine.classList.add('third', 'divs');
     container.appendChild(thirdLine);
 
-const thirdFirst = document.createElement('div');
-    thirdFirst.classList.add('thirdFirst', 'square');
-    thirdLine.appendChild(thirdFirst);
-
-const thirdSecond = document.createElement('div');
-    thirdSecond.classList.add('thirdSecond', 'square');
-    thirdLine.appendChild(thirdSecond);
-
-const thirdThird = document.createElement('div');
-    thirdThird.classList.add('thirdThird', 'square');
-    thirdLine.appendChild(thirdThird);
-
-const thirdFourth = document.createElement('div');
-    thirdFourth.classList.add('thirdFourth', 'square');
-    thirdLine.appendChild(thirdFourth);
+    for(let i3 = 1; i3 <= 16; i3++) {
+        first[i3] = document.createElement('div');
+        first[i3].classList.add(`second${i3}`, 'square');
+        thirdLine.appendChild(first[i3]);}
 
 // ------------- 4th Line -------------
 const fourthLine = document.createElement('div');
     fourthLine.classList.add('fourth', 'divs');
     container.appendChild(fourthLine);
 
-const fourthFirst = document.createElement('div');
-    fourthFirst.classList.add('fourthFirst', 'square');
-    fourthLine.appendChild(fourthFirst);
-
-const fourthSecond = document.createElement('div');
-    fourthSecond.classList.add('fourthSecond', 'square');
-    fourthLine.appendChild(fourthSecond);
-
-const fourthThird = document.createElement('div');
-    fourthThird.classList.add('fourthThird', 'square');
-    fourthLine.appendChild(fourthThird);
-
-const fourthFourth = document.createElement('div');
-    fourthFourth.classList.add('fourthFourth', 'square');
-    fourthLine.appendChild(fourthFourth);
+    for(let i4 = 1; i4 <= 16; i4++) {
+        first[i4] = document.createElement('div');
+        first[i4].classList.add(`second${i4}`, 'square');
+        fourthLine.appendChild(first[i4]);}
