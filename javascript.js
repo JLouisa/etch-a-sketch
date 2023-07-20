53da4eb34bddbae68061517336b8422465510041
Jonathan Louisa <jonathan.louisa@gmail.com>
2023-07-20 12:10:43 -0400

    Optimized code to two loops for the 16x16 grid

--- javascript.js
+++ javascript.js
@@ -1,12 +1,14 @@
 const container = document.querySelector('.middle');
 
-// ------------- 1st Line -------------
-const firstLine = document.createElement('div');
-    firstLine.classList.add('first', 'divs');
-    container.appendChild(firstLine);
+let divs = []
+for(n = 1; n <= 16; n++) {
+    divs[n] = document.createElement('div');
+    divs[n].classList.add(`head${n}`, 'divs');
+    container.appendChild(divs[n]);
 
-    let first = [];
-    for(let i1 = 1; i1 <= 16; i1++) {
-        first[i1] = document.createElement('div');
-        first[i1].classList.add(`first${i1}`, 'square');
-        firstLine.appendChild(first[i1]);}
+        let first = [];
+        for(let i1 = 1; i1 <= 16; i1++) {
+            first[i1] = document.createElement('div');
+            first[i1].classList.add(`first${i1}`, 'square');
+            divs[n].appendChild(first[i1]);}
+}
