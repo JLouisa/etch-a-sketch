715c2fd6338d329ab3556873effdf4927c08e682
Jonathan Louisa <jonathan.louisa@gmail.com>
2023-07-20 11:00:35 -0400

    Add 16 div through js and removed from html

---
+++ javascript.js
@@ -0,0 +1,85 @@
+const container = document.querySelector('.middle');
+
+// ------------- 1st Line -------------
+const firstLine = document.createElement('div');
+    firstLine.classList.add('first', 'divs');
+    container.appendChild(firstLine);
+
+const firstFirst = document.createElement('div');
+    firstFirst.classList.add('firstFirst');
+    firstLine.appendChild(firstFirst);
+
+const firstSecond = document.createElement('div');
+    firstSecond.classList.add('firstSecond');
+    firstLine.appendChild(firstSecond);
+
+const firstThird = document.createElement('div');
+    firstThird.classList.add('firstThird');
+    firstLine.appendChild(firstThird);
+
+const firstFourth = document.createElement('div');
+    firstFourth.classList.add('firstFourth');
+    firstLine.appendChild(firstFourth);
+
+    // ------------- 2nd Line -------------
+const secondLine = document.createElement('div');
+    secondLine.classList.add('second', 'divs');
+    container.appendChild(secondLine);
+
+const secondFirst = document.createElement('div');
+    secondFirst.classList.add('secondFirst');
+    secondLine.appendChild(secondFirst);
+
+const secondSecond = document.createElement('div');
+    secondSecond.classList.add('secondSecond');
+    secondLine.appendChild(secondSecond);
+
+const secondThird = document.createElement('div');
+    secondThird.classList.add('secondThird');
+    secondLine.appendChild(secondThird);
+
+const secondFourth = document.createElement('div');
+    secondFourth.classList.add('secondFourth');
+    secondLine.appendChild(secondFourth);
+
+// ------------- 3rd Line -------------
+const thirdLine = document.createElement('div');
+thirdLine.classList.add('third', 'divs');
+    container.appendChild(thirdLine);
+
+const thirdFirst = document.createElement('div');
+thirdFirst.classList.add('thirdFirst');
+    thirdLine.appendChild(thirdFirst);
+
+const thirdSecond = document.createElement('div');
+thirdSecond.classList.add('thirdSecond');
+    thirdLine.appendChild(thirdSecond);
+
+const thirdThird = document.createElement('div');
+thirdThird.classList.add('thirdThird');
+    thirdLine.appendChild(thirdThird);
+
+const thirdFourth = document.createElement('div');
+thirdFourth.classList.add('thirdFourth');
+    thirdLine.appendChild(thirdFourth);
+
+// ------------- 4th Line -------------
+const fourthLine = document.createElement('div');
+    fourthLine.classList.add('fourth', 'divs');
+    container.appendChild(fourthLine);
+
+const fourthFirst = document.createElement('div');
+    fourthFirst.classList.add('fourthFirst');
+    fourthLine.appendChild(fourthFirst);
+
+const fourthSecond = document.createElement('div');
+    fourthSecond.classList.add('fourthSecond');
+    fourthLine.appendChild(fourthSecond);
+
+const fourthThird = document.createElement('div');
+    fourthThird.classList.add('fourthThird');
+    fourthLine.appendChild(fourthThird);
+
+const fourthFourth = document.createElement('div');
+    fourthFourth.classList.add('fourthFourth');
+    fourthLine.appendChild(fourthFourth);
